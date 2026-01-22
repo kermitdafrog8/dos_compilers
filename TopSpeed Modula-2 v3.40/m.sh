64f786aae8ee918287ca693d9a22db45b1011fe9
davidly <davidly_lee@hotmail.com>
2026-01-22 07:46:20 -0800

    linux build

---
+++ TopSpeed Modula-2 v3.40/m.sh
@@ -0,0 +1,9 @@
+str=$(tr '[a-z]' '[A-Z]' <<< $1)
+
+# build using a project file
+ntvdm -u -r:. -e:PATH=c:\\SYS SYS\\TSC /m $str.pr
+
+rm TERMINAL.OBJ 2>/dev/null
+rm ASCII.OBJ 2>/dev/null
+rm INOUT.OBJ 2>/dev/null
+rm $str.OBJ 2>/dev/null
